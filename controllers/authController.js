1855864d11572410de3abdac5e700de5c80ef912
sriharsha <sriharshadabbiru04@gmail.com>
2025-07-29 10:50:57 +0530

    authentication

---
+++ controllers/authController.js
@@ -0,0 +1,80 @@
+const pool = require("../config/db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.signup = async (req, res) => {
+  const { email, password, name } = req.body;
+
+  // input validity
+  if (!email || !password || !name) {
+    return res.status(400).json({ message: "Please fill all the fields" });
+  }
+
+  //check if user exists
+  const userExists = await pool.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+  if (userExists.rows.length > 0) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  //hash password
+  const hashed = await bcrypt.hash(password, 10);
+
+  //inserting the user into the db
+  const newUser = await pool.query(
+    "INSERT INTO users (email, password, name) VALUES ($1, $2, $3) RETURNING id,email,name,is_admin",
+    [email, hashed, name]
+  );
+
+  //create JWT token
+  const token = jwt.sign(
+    { userId: newUser.rows[0].id, is_admin: newUser.rows[0].is_admin },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+
+  res.status(201).json({
+    ...newUser.rows[0],
+    token,
+  });
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  // input validity
+  if (!email || !password) {
+    return res.status(400).json({ message: "Please fill all the fields" });
+  }
+
+  //fetch user from db
+  const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+  const user = result.rows[0];
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  //check password
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  //create JWT token
+  const token = jwt.sign(
+    { userId: user.id, is_admin: user.is_admin },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+
+  res.status(200).json({
+    id: user.id,
+    email: user.email,
+    name: user.name,
+    is_admin: user.is_admin,
+    token,
+  });
+};
--- server.js
+++ server.js
@@ -1,16 +1,36 @@
-const express = require('express');
-const dotenv= require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const pool = require("./config/db");
 dotenv.config();
 
-const app= express();
+const app = express();
+const authRoutes = require("./routes/auth");
+
+process.on("unhandledRejection", (err) => {
+  console.error("❌ Unhandled Rejection:", err.message);
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.error("❌ Uncaught Exception:", err.message);
+  process.exit(1);
+});
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('LMS api is working');
+app.get("/", (req, res) => {
+  res.send("LMS api is working");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.use("/api/auth", authRoutes);
+
+console.log("Loaded PORT:", process.env.PORT);
+const PORT = 5000; // use hardcoded value for now
+
+try {
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+  });
+} catch (err) {
+  console.error(" Server failed to start:", err);
+}
